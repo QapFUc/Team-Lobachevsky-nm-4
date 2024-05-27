d826d1e7fbc58fd037e447d89be900ea2e00f809
Vladislav Suchkov <v.suchkov@yadro.com>
2024-05-27 18:11:54 +0300

    vectors

--- src/core/src/CGM.cpp
+++ src/core/src/CGM.cpp
@@ -2,79 +2,17 @@
 #include "dataTypes/common.hpp"
 #include <cmath>
 #include <cstddef>
+#include <ctime>
 #include <iostream>
 #include <numeric>
+#include <ostream>
 #include <vector>
-
-static void MatrixOperate(const std::vector<double>& A, const std::vector<double>& x, std::vector<double>& res, const size_t& width) {
-    size_t i = 0, j = 0;
-    size_t k = 0;
-    double cX[5];
-    for (size_t m = 0; m < x.size(); ++m) {
-        i = m % width;
-        j = m / width;
-        k = 5 * m;
-        cX[2] = x[i + j * width];
-
-        j = j - 1;
-        if (j < 0) {
-            cX[0] = 0.l;
-        } else {
-            cX[0] = x[i + j * width];
-        }
-
-        i = i - 1;
-        j += 1;
-        if (i < 0) {
-            cX[1] = 0.l;
-        } else {
-            cX[1] = x[i + j * width];
-        }
-
-        i = i + 2;
-        if (i + j * width >= x.size()) {
-            cX[3] = 0.l;
-        } else {
-            cX[3] = x[i + j * width];
-        }
-
-        i = i - 1;
-        j = j + 1;
-        if (i + j * width >= x.size()) {
-            cX[4] = 0.l;
-        } else {
-            cX[4] = x[i + j * width];
-        }
-
-        res[m] = A[k] * cX[0] + A[k + 1] * cX[1] + A[k + 2] * cX[2] + A[k + 3] * cX[3] + A[k + 4] * cX[4];
-    }
-}
-
-static void VectorSum(const std::vector<double>& x, const std::vector<double>& y, std::vector<double>& res, const double& a = 1, const double& b = 1) {
-    for (size_t k = 0; k < x.size(); ++k) {
-        res[k] = a * x[k] + b * y[k];
-    }
-}
-
-static void VectorSub(const std::vector<double>& x, const std::vector<double>& y, std::vector<double>& res, const double& a = 1, const double& b = 1) {
-    for (size_t k = 0; k < x.size(); ++k) {
-        res[k] = a * x[k] - b * y[k];
-    }
-}
-
-static double InnerProd(const std::vector<double>& x, const std::vector<double>& y) {
-    double res = 0.l;
-    for (size_t k = 0; k < x.size(); ++k) {
-        res += x[k] * y[k];
-    }
-    return res;
-}
-
-static double EuclidianNormSq(const std::vector<double>& x) {
-    return InnerProd(x, x);
-}
+#include "core/vector.hpp"
+#include "nm/utils/logger.hpp"
+#include "nm/utils/profiler.hpp"
 
 std::vector<double> ConjGradMethod::eval() {
+    LOG_INFO_CLI("Creating vectors");
     std::vector<double> result(rhs->size());
     std::vector<double> direction(rhs->size());
     std::vector<double> residual(rhs->size());
@@ -82,11 +20,13 @@ std::vector<double> ConjGradMethod::eval() {
 
     // Start residual
 
+    LOG_INFO_CLI("Set start approach");
     if (cfg.startX == nm::StartApr::Zeros) {
         for (auto& x : result)
             x = 0.l;
     }
 
+    LOG_INFO_CLI("Evaluating start residual");
     MatrixOperate(*matrix, result, residual, cfg.matrix_width);
     VectorSub(*rhs, residual, residual);
 
@@ -96,8 +36,15 @@ std::vector<double> ConjGradMethod::eval() {
 
     double curr_tol = 1e15;
     size_t k = 0;
+    double percentage = 0.l;
+    double perc_step = (1.l / cfg.Max_N);
     std::vector<double> tmp(rhs->size());
+    LOG_INFO_CLI("Iterating CGM...");
+    std::cout << percentage * 100.l << "\% done ";
+    std::clock_t start;
+    double duration = 0.l;
     while (curr_tol >= cfg.tolerance && k < cfg.Max_N) {
+        start = std::clock();
         MatrixOperate(*matrix, direction, tmp, cfg.matrix_width);
         prod = InnerProd(residual, residual);
         alpha = prod / InnerProd(direction, tmp);
@@ -110,6 +57,14 @@ std::vector<double> ConjGradMethod::eval() {
         VectorSum(residual, direction, direction, 1.l, beta);
         curr_tol = std::sqrt(EuclidianNormSq(residual));
         k++;
+
+        duration = static_cast<double>(std::clock() - start) / CLOCKS_PER_SEC;
+        percentage += perc_step;
+        std::cout << '\r' << percentage * 100.l << "\% done " << ". Estimation time (approx): " << duration * (cfg.Max_N - k) << " s" <<  std::flush;
     }
+
+    std::cout << '\n';
+    std::cout << "Total tolerance: " << curr_tol << '\n';
+    LOG_INFO_CLI("CGM finished");
     return result;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -8,22 +8,26 @@
 #include <iostream>
 #include <vector>
 
-static void GenLSETestFunc(const std::string& fname) {
+static void GenLSETestFunc(const std::string& fname, nm::Profiler& prof) {
+    nm::Profiler nest = prof.nest(nm::GET_CURRENT_SCOPE());
     NetPattern pat = ImportNetPattern(fname);
-    Net net = pat.generateNet(10, 0, 0, 0.1, 0.1);
+    LOG_INFO_CLI("Got pattern");
+    Net net = pat.generateNet(5000, 0, 0, 0.1, 0.1);
 
-    for (size_t j = 0; j < net.nodes[0].size(); ++j) {
-        for (size_t i = 0; i < net.nodes.size(); ++i) {
-            if (net.nodes[i][j] == NodeType::BOUND) {
-                std::cout << 'o';
-            } else if (net.nodes[i][j] == NodeType::INNER) {
-                std::cout << 'x';
-            } else {
-                std::cout << ' ';
-            }
-        }
-        std::cout << '\n';
-    }
+    LOG_INFO_CLI("Net generated");
+
+    // for (size_t j = 0; j < net.nodes[0].size(); ++j) {
+    //     for (size_t i = 0; i < net.nodes.size(); ++i) {
+    //         if (net.nodes[i][j] == NodeType::BOUND) {
+    //             std::cout << 'o';
+    //         } else if (net.nodes[i][j] == NodeType::INNER) {
+    //             std::cout << 'x';
+    //         } else {
+    //             std::cout << ' ';
+    //         }
+    //     }
+    //     std::cout << '\n';
+    // }
     auto fb = [](const double& x, const double& y) -> double {
         return (x + y);
     };
@@ -33,66 +37,73 @@ static void GenLSETestFunc(const std::string& fname) {
 
     Config cfg;
     cfg.startX = nm::StartApr::Zeros;
-    cfg.tolerance = 1e-6;
-    cfg.Max_N = 10000;
+    cfg.tolerance = 1e-15;
+    cfg.Max_N = 50;
 
     DirichletTask dt(fb, fr, net, cfg);
 
+    LOG_INFO_CLI("Genetaring LS");
     dt.GenerateLinearSystem();
     auto lscoefs = dt.Coefs();
     auto lsrhs = dt.Rhs();
+    LOG_INFO_CLI("LS generated");
 
     size_t width = net.nodes.size();
     size_t height = net.nodes[0].size();
 
-    std::cout << "Matrix" << '\n';
+    // std::cout << "Matrix" << '\n';
 
-    std::cout << lscoefs[0] << ' ';
-    size_t k = 0;
-    for (size_t i = 1; i < lscoefs.size(); ++i) {
-        if (i % 5 == 0) {
-            std::cout << "| " << lsrhs[k];
-            k++;
-            std::cout << '\n';
-        }
-        std::cout << lscoefs[i] << ' ';
-    }
-    std::cout << "| " << lsrhs[k];
-    std::cout << '\n';
+    // std::cout << lscoefs[0] << ' ';
+    // size_t k = 0;
+    // for (size_t i = 1; i < lscoefs.size(); ++i) {
+    //     if (i % 5 == 0) {
+    //         std::cout << "| " << lsrhs[k];
+    //         k++;
+    //         std::cout << '\n';
+    //     }
+    //     std::cout << lscoefs[i] << ' ';
+    // }
+    // std::cout << "| " << lsrhs[k];
+    // std::cout << '\n';
+
+    std::cout << "Var vector size: " << dt.Rhs().size() << '\n';
 
     dt.SetMethod(nm::Method::CGM);
+    LOG_INFO_CLI("Evaluating tasak...");
     dt.eval();
-    std::vector<double> sol1(dt.Solution().size());
-    sol1 = dt.Solution();
+    LOG_INFO_CLI("Task evaluated");
+    // std::vector<double> sol1(dt.Solution().size());
+    // sol1 = dt.Solution();
 
     std::cout << "CGM solution\n";
-    for (auto& x : sol1) {
-        std::cout << x << '\n';
+    for (size_t i = 0; i < 100; ++i) {
+        std::cout << dt.Solution()[i] << '\n';
     }
 
-    dt.SetMethod(nm::Method::SimpleIter);
-    dt.eval();
+    // dt.SetMethod(nm::Method::SimpleIter);
+    // dt.eval();
 
-    std::vector<double> sol2(dt.Solution().size());
-    sol2 = dt.Solution();
+    // std::vector<double> sol2(dt.Solution().size());
+    // sol2 = dt.Solution();
 
-    std::cout << "SIM solution\n";
-    for (auto& x : sol2) {
-        std::cout << x << '\n';
-    }
+    // std::cout << "SIM solution\n";
+    // for (auto& x : sol2) {
+    //     std::cout << x << '\n';
+    // }
 
-    double diff = 0.l;
-    double curr_diff = 0.l;
-    for (size_t i = 0; i < sol1.size(); ++i) {
-        curr_diff = std::abs(sol1[i] - sol2[i]);
-        if (curr_diff > diff)
-            diff = curr_diff;
-    }
+    // double diff = 0.l;
+    // double curr_diff = 0.l;
+    // for (size_t i = 0; i < sol1.size(); ++i) {
+    //     curr_diff = std::abs(sol1[i] - sol2[i]);
+    //     if (curr_diff > diff)
+    //         diff = curr_diff;
+    // }
 
-    std::cout << "Diff betw CGM and SIM: " << diff << '\n';
+    // std::cout << "Diff betw CGM and SIM: " << diff << '\n';
 }
 
 int main(int argc, char* argv[]) {
-    GenLSETestFunc(argv[1]);
+    nm::Profiler prof;
+    GenLSETestFunc(argv[1], prof);
     return 0;
 }
